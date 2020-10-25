07516a5d27474ba28b37f0a5d1948012369d763e
Jumaolili <57054676+Jumaolili@users.noreply.github.com>
2020-10-25 20:52:35 +0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+﻿
+var crews = document.querySelectorAll("#test");
+
+
+for (let i = 0; i < crews.length; i++) {
+    crews.onClick = function () {
+        let lis = this.document.querySelectorAll("div");
+        lis[1].setAttribute("class", "page6-Active");
+    }
+    crews.onmouseout = function () {
+        let lis = this.document.querySelectorAll("div");
+        lis[1].setAttribute("class", "page6-NonActive");
+    }
+}
